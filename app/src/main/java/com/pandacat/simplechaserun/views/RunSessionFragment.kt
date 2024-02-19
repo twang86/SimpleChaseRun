a249ba4b43c978503df0d8466ed443b938e8c58b
Tim <y1wang86@gmail.com>
2024-02-19 16:21:59 -0500

    added mapview

--- app/src/main/java/com/pandacat/simplechaserun/views/RunSessionFragment.kt
+++ app/src/main/java/com/pandacat/simplechaserun/views/RunSessionFragment.kt
@@ -5,15 +5,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
 import com.pandacat.simplechaserun.databinding.FragmentRunSessionBinding
 
 class RunSessionFragment : Fragment() {
     lateinit var binding: FragmentRunSessionBinding
+    private var map: GoogleMap? = null;
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View
     {
         binding = FragmentRunSessionBinding.inflate(inflater, container, false)
         setupUI()
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync{
+            map = it;
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
     }
 
     private fun setupUI()
